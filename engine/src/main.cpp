a885d5ac933b626b2b136e3ae8785166dd9a5df4
Jesse Stolwijk <jesse.stolwijk@ximedes.com>
2021-11-11 18:46:46 +0100

    Control the motor

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,14 +1,34 @@
 #include <Arduino.h>
 
-#define ONBOARD_LED 2
+int motor1pin1 = 2;
+int motor1pin2 = 3;
 
-void setup() {
-  pinMode(ONBOARD_LED, OUTPUT);
+int motor2pin1 = 4;
+int motor2pin2 = 5;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
   delay(1000);
-  digitalWrite(ONBOARD_LED, HIGH);
-  delay(100);
-  digitalWrite(ONBOARD_LED, LOW);
+
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  delay(1000);
 }
