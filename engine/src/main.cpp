9395e584bb82cd3432d83e94013b860ef2deb39a
Jesse Stolwijk <jesse.stolwijk@ximedes.com>
2022-03-01 18:09:43 +0100

    Update

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
 int motor1pin1 = 2;
-int motor1pin2 = 3;
+int motor1pin2 = 4;
 
-int motor2pin1 = 4;
-int motor2pin2 = 5;
+// int motor2pin1 = 4;
+// int motor2pin2 = 5;
 
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1, OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
+  // pinMode(motor2pin1, OUTPUT);
+  // pinMode(motor2pin2, OUTPUT);
 }
 
 void loop()
@@ -21,14 +21,14 @@ void loop()
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
 
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-  delay(1000);
+  // digitalWrite(motor2pin1, HIGH);
+  // digitalWrite(motor2pin2, LOW);
+  delay(5000);
 
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
 
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  delay(1000);
+  // digitalWrite(motor2pin1, LOW);
+  // digitalWrite(motor2pin2, HIGH);
+  // delay(1000);
 }
