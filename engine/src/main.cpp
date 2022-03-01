505b58687e6f222ccd6e60de28355145bc69d8e8
Jesse Stolwijk <jesse.stolwijk@ximedes.com>
2022-03-01 18:29:31 +0100

    Nice

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,34 +1,39 @@
 #include <Arduino.h>
 
 int motor1pin1 = 2;
-int motor1pin2 = 4;
 
-// int motor2pin1 = 4;
-// int motor2pin2 = 5;
+// the number of the LED pin
+const int ledPin = 2; // 16 corresponds to GPIO16
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(motor1pin1, OUTPUT);
-  pinMode(motor1pin2, OUTPUT);
-  // pinMode(motor2pin1, OUTPUT);
-  // pinMode(motor2pin2, OUTPUT);
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
+  // increase the LED brightness
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++)
+  {
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
 
-  // digitalWrite(motor2pin1, HIGH);
-  // digitalWrite(motor2pin2, LOW);
-  delay(5000);
-
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-
-  // digitalWrite(motor2pin1, LOW);
-  // digitalWrite(motor2pin2, HIGH);
-  // delay(1000);
+  // decrease the LED brightness
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--)
+  {
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
 }
